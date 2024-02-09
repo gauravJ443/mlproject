b25ed6000db021fcd4e7e8fe6f6a206b77a3d70d
Gaurav <gauravj443@gmail.com>
2024-02-09 15:32:23 +0530

    Data Ingestion

---
+++ src/components/data_ingestion.py
@@ -0,0 +1,56 @@
+import os
+import sys
+from src.exception import CustomException
+from src.logger import logging
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from dataclasses import dataclass
+
+
+@dataclass                                   #this is the decorator
+class DataIngestionConfig:                    #so now dataingestion component know where to save my trainpath,testpath and raw data path as we had given the inputs
+    train_data_path: str = os.path.join('artifacts',"train.csv")
+    test_data_path: str = os.path.join('artifacts',"test.csv")
+    raw_data_path: str = os.path.join('artifacts',"data.csv")
+    
+#if we have to only define the variable then we could have used dataclass 
+# but if we have some other function inside the class then we will use some constructor inside the class 
+class DataIngestion:
+    def __init__(self):
+        self.ingestion_config = DataIngestionConfig() #ingestion_config variable will consist of all this 3 values present in DataIngestionConfig.
+        
+    def initiate_data_ingestion(self):    #if some data is stored somewhwere i will write code inside it this function will work 
+        logging.info("Entered the data ingestion method or component")
+        try:
+            df = pd.read_csv("notebook\data\stud.csv")
+            logging.info("Read the dataset as dataframe") #we are using logging many times because whenever the exception rise we can easily know where it happened
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True) #making directories and checking if it exist then use that only
+            
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+            
+            logging.info("Train Test Split initiated")
+            train_set,test_set = train_test_split(df,test_size=0.2,random_state=42) #from this we will get train and test set from the df
+            
+            train_set.to_csv(self.ingestion_config.train_data_path,index = False,header=True)
+            
+            test_set.to_csv(self.ingestion_config.test_data_path,index = False,header=True)
+            
+            logging.info("Ingestion of data is completed")
+            
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+                
+            )
+            
+        except Exception as e :
+            raise CustomException(e,sys)
+        
+if __name__ == "__main__":
+    obj= DataIngestion()
+    obj.initiate_data_ingestion()
+        
+    
+    
+                         
+    
